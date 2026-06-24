cebe23d75a0179b5eeef6073db2bef6692c962ef
Shivam Tiwari <shivamtiwarix88@gmail.com>
2026-06-24 22:46:03 +0530

    updated app.py for deployment

--- ml-model/app.py
+++ ml-model/app.py
@@ -11,12 +11,16 @@ heart_rate = st.number_input("Heart Rate", 30, 250)
 systolic = st.number_input("Systolic Blood Pressure", 50, 250)
 diastolic = st.number_input("Diastolic Blood Pressure", 30, 150)
 blood_sugar = st.number_input("Blood Sugar", 0, 500)
+gender = st.selectbox("Gender", ["male", "female"])
 ck_mb = st.number_input("CK-MB")
 troponin = st.number_input("Troponin")
+
+gender_encoded = 1 if gender == "male" else 0
 
 if st.button("Predict"):
     data = pd.DataFrame([{
         "Age": age,
+        "Gender": gender_encoded,
         "Heart rate": heart_rate,
         "Systolic blood pressure": systolic,
         "Diastolic blood pressure": diastolic,
